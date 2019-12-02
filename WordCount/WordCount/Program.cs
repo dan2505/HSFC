cb1bb8098b032f78db6406ad9f4e39c78dce97f2
Dan J Williams <djw9349@hereford.ac.uk>
2019-12-02 09:57:03 +0000

    Add WordCount

---
+++ WordCount/WordCount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string:");
+            string[] words = Console.ReadLine().Split(" ");
+
+            WordCounter dictionary = new WordCounter(words.Length);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                dictionary.AddWordToList(words[i].ToLower());
+            }
+
+            for (int i = 0; i < dictionary.GetNextFreeLocation(); i++)
+            {
+                Console.WriteLine(dictionary.GetList()[i].GetWord() + " - " + dictionary.GetList()[i].GetCount());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WordCount/WordCount/WordCounter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace WordCount
+{
+    class WordCounter
+    {
+        private WordCountPair[] collection;
+        private int nextFreeLocation;
+
+        public WordCounter(int maxSize)
+        {
+            collection = new WordCountPair[maxSize];
+        }
+
+        public int AddWordToList(string theWord)
+        {
+            if (nextFreeLocation > collection.Length) return -1;
+
+            int position = 0;
+            while ((position < nextFreeLocation) && (string.Compare(theWord, collection[position].GetWord(), StringComparison.Ordinal) > 0))
+            {
+                position++;
+            }
+
+            for (int i = nextFreeLocation; i > position; i--)
+            {
+                collection[i] = collection[i - 1];
+            }
+
+            for (int i = 0; i < nextFreeLocation; i++)
+            {
+                if (collection[i].GetWord() == theWord)
+                {
+                    collection[i].SetCount(collection[i].GetCount() + 1);
+                    return i;
+                }
+            }
+
+            collection[position] = new WordCountPair(theWord, 1);
+            nextFreeLocation++;
+            return position;
+        }
+
+        public int GetNextFreeLocation()
+        {
+            return nextFreeLocation;
+        }
+
+        public WordCountPair[] GetList()
+        {
+            return collection;
+        }
+
+    }
+}
