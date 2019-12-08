e61dfd14f5a3773594f4d4a089a1c38bdbc3e354
dan2505 <57019164+dan2505@users.noreply.github.com>
2019-12-08 17:50:14 +0000

    Add stacks

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(100);
+
+            stack.Push(13);
+            stack.Push(7);
+            stack.Push(5);
+
+            Console.WriteLine(stack.Peek() + " - " + stack.Depth());
+
+            stack.Pop();
+
+            Console.WriteLine(stack.Peek() + " - " + stack.Depth());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,50 @@
+﻿namespace Stack
+{
+    public class Stack
+    {
+        private int top;
+        private readonly object[] stack;
+
+        public Stack(int size)
+        {
+            stack = new object[size];
+        }
+
+        public bool Push(object obj)
+        {
+            if (top < stack.Length)
+            {
+                top++;
+                stack[top] = obj;
+
+                return true;
+            }
+            return false;
+        }
+
+        public object Pop()
+        {
+            if (top > 0)
+            {
+                top--;
+                return stack[top];
+            }
+            return null;
+        }
+
+        public int Depth()
+        {
+            return top;
+        }
+
+        public object Peek()
+        {
+            return stack[top];
+        }
+
+        public bool IsEmpty()
+        {
+            return top == 0;
+        }
+    }
+}
