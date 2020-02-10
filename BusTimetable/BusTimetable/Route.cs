34e756fc404f7189d6f944259117bc348e27fb2c
Dan J Williams <djw9349@hereford.ac.uk>
2020-02-10 09:49:00 +0000

    Start bus timetable

---
+++ BusTimetable/BusTimetable/Route.cs
@@ -0,0 +1,30 @@
+﻿namespace BusTimetable
+{
+    class Route
+    {
+        private string origin, destination;
+        private string[] startTimes;
+
+        public Route(string origin, string destination, string[] startTimes)
+        {
+            this.origin = origin;
+            this.destination = destination;
+            this.startTimes = startTimes;
+        }
+
+        public string getOrigin()
+        {
+            return origin;
+        }
+
+        public string getDestination()
+        {
+            return destination;
+        }
+
+        public string[] getStartTimes()
+        {
+            return startTimes;
+        }
+    }
+}
